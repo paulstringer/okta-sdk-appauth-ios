f12115a5f7ea9e9f76f6df8d51d29206494f16c3
paulstringer <pstringer@equalexperts.com>
2017-11-24 10:42:19 +0000

    Adds Carthage Support

---
+++ OktaAuth/OktaAuth.h
@@ -0,0 +1,19 @@
+//
+//  OktaAuth.h
+//  OktaAuth
+//
+//  Created by paulstringer on 24/11/2017.
+//  Copyright © 2017 Equal Experts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OktaAuth.
+FOUNDATION_EXPORT double OktaAuthVersionNumber;
+
+//! Project version string for OktaAuth.
+FOUNDATION_EXPORT const unsigned char OktaAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OktaAuth/PublicHeader.h>
+
+
